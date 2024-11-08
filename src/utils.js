50937685630b0ae61ce5519dec064e915f44793e
xlolo_id <64420551+lxnrdn@users.noreply.github.com>
2024-11-08 14:43:47 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,14 +4,14 @@ const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function displayHeader() {
     process.stdout.write('\x1Bc');
-    console.log('  ____       _            _         _                 '.cyan);
-    console.log(' |  _ \     | |     /\   (_)       | |                '.cyan);
-    console.log(' | |_) |    | |    /  \   _ _ __ __| |_ __ ___  _ __  '.cyan);
-    console.log(' |  _ < _   | |   / /\ \ | | '__/ _` | '__/ _ \| '_ \ '.cyan);
-    console.log(' | |_) | |__| |  / ____ \| | | | (_| | | | (_) | |_) |'.cyan);
-    console.log(' |____/ \____/  /_/    \_\_|_|  \__,_|_|  \___/| .__/ '.cyan);
-    console.log('                                               | |    '.cyan);
-    console.log('                                               |_|    '.cyan);
+    console.log('  ____       _            _         _                  '.cyan);
+    console.log(' |  _ \     | |     /\   (_)       | |                 '.cyan);
+    console.log(' | |_) |    | |    /  \   _ _ __ __| |_ __ ___  _ __   '.cyan);
+    console.log(' |  _ < _   | |   / /\ \ | | |__/ _` | |__/ _ \| |_ \  '.cyan);
+    console.log(' | |_) | |__| |  / ____ \| | | | (_| | | | (_) | |_) | '.cyan);
+    console.log(' |____/ \____/  /_/    \_\_|_|  \__,_|_|  \___/| .__/  '.cyan);
+    console.log('                                               | |     '.cyan);
+    console.log('                                               |_|     '.cyan);
     console.log();
   }
 
