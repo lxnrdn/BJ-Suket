3191ed5f5d3343ca51aa1db144bc9d077d37b5e7
xlolo_id <64420551+lxnrdn@users.noreply.github.com>
2024-11-09 11:38:42 +0700

    Delete src directory

--- src/Config.js
+++
@@ -1,9 +0,0 @@
-class Config {
-  constructor() {
-    this.ipCheckURL = 'https://ipinfo.io/json';
-    this.wssHost = 'proxy.wynd.network:4444';
-    this.retryInterval = 20000;
-  }
-}
-
-module.exports = Config;
